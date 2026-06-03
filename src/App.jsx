c04ec294e3e3253a60f6bbd86cfc61567c999212
SparklingStar212 <deborahalabi49@gmail.com>
2026-06-03 07:57:02 +0100

    feat: add CardListing component for displaying property details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import CardListing from './components/CardListing'
 
 const App = () => {
   return (
