cc6e0b86dc3bdd2e53a181999d7f04399a1f1882
inioluwa <ajayiinioluwa2007@gmail.com>
2026-06-03 07:26:35 +0100

    chore: clear temporary preview code from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import PropertyDetails from './pages/PropertyDetails'
 
 const App = () => {
   return (
     <>
-      <PropertyDetails />
+      
     </>
   )
 }
