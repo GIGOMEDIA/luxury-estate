9008d329ab87eb9045b62deecb09c66ede2e78e9
inioluwa <ajayiinioluwa2007@gmail.com>
2026-06-06 12:21:18 +0100

    chore: clear temporary homepage preview from App.jsx entry point

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import CardListing from './components/CardListing'
-import Home from './pages/Home'
 
 const App = () => {
   return (
     <>
-      <Home />
+    
     </>
   )
 }
