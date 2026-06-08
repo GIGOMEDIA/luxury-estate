636d602f35bcc4ca49e924347c316fe61f893217
SparklingStar212 <deborahalabi49@gmail.com>
2026-06-08 11:11:48 +0100

    feat: update home route to render Home component instead of redirecting

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import AgentProfile from './pages/AgentProfile'
 import PropertyDetails from './pages/PropertyDetails'
 import PropertySearch from './pages/PropertySearch'
 import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/properties" replace />} />
+      <Route path="/" element={<Home />} />
       <Route path="/properties" element={<PropertySearch />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/agent-profile" element={<AgentProfile />} />
