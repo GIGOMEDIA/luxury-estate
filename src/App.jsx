7b6ac066c9d2efa6a2f6a4de7578a836c35fac43
SparklingStar212 <deborahalabi49@gmail.com>
2026-07-17 12:21:57 +0100

    feat: add Concierge Services page with related components and routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import PropertySearch from './pages/PropertySearch'
 import Dashboard from './pages/Dashboard'
 import AdvisoryAndAgents from './pages/AdvisoryAndAgents'
 import CuratedCollections from './pages/CuratedCollections'
+import ConciergeServices from './pages/ConciergeServices'
 import Home from './pages/Home'
 
 const App = () => {
@@ -14,6 +15,8 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/properties" element={<PropertySearch />} />
       <Route path="/collections" element={<CuratedCollections />} />
+      <Route path="/concierge" element={<ConciergeServices />} />
+      <Route path="/Concierge" element={<ConciergeServices />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/agents" element={<AdvisoryAndAgents />} />
       <Route path="/agent-profile" element={<AgentProfile />} />
